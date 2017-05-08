e7b58cd4d68a518f6a73810c70cde4c93549b279
Kiet Huynh <huynhchanhkiet94@gmail.com>
2017-05-09 00:57:21 +0700

    Update csproj

--- Source Code/C#/CardProcessingApi/CardProcessingApi.Web/Framework/IdentityModels.cs
+++ Source Code/C#/CardProcessingApi/CardProcessingApi.Web/Framework/IdentityModels.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
 
 namespace CardProcessingApi.Web.Framework
 {
     public class IdentityModels
     {
     }
+
+    public class ApplicationUser : IUser<String>
+    {
+        public string Id { get; }
+        public string UserName { get; set; }
+    }
+
+    public 
 }
