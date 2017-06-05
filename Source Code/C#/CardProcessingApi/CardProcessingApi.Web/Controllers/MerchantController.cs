fbd18a3f17a2bac6a63a20ac3619cbecea372c7c
vinhnam25 <tvnam259@gmail.com>
2017-06-05 19:21:58 +0700

    Add MerchantController
    Chức năng 1

---
+++ Source Code/C#/CardProcessingApi/CardProcessingApi.Web/Controllers/MerchantController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using AutoMapper;
+using CardProcessing.Business.BusinessLogic.AgentLogic;
+using CardProcessingApi.Core;
+using CardProcessingApi.Data;
+using CardProcessingApi.Web.Framework;
+using CardProcessingApi.Web.Framework.Extension;
+using CardProcessingApi.Web.Framework.Filters;
+using CardProcessingApi.Web.Models;
+
+namespace CardProcessingApi.Web.Controllers
+{
+    public class MerchantController : BaseApiController
+    {
+        public MerchantController()
+        {
+        }
+
+        [HttpPost]
+        [RoleAuthorize(Enums.UserRole.Master)]
+        [Route("active/{merchantID}")]
+        public IHttpActionResult ActivateMerchant(int merchantID)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [RoleAuthorize(Enums.UserRole.Master)]
+        [Route("inactive/{merchantID}")]
+        public IHttpActionResult InactivateMerchant(int merchantID)
+        {
+            return Ok();
+        }
+    }
+}
