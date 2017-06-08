42652c5ac0774fab39f5a150d336091746fbff03
vinhnam25 <tvnam259@gmail.com>
2017-06-08 16:34:45 +0700

    Revert "Revert "Active/Unactive Merchant has been DONE. Update Merchant class, model.""
    This reverts commit 1ece28ea7d50211d2c5618db0663b8665367c63e.

--- Source Code/C#/CardProcessingApi/CardProcessingApi.Web/Controllers/MerchantController.cs
+++ Source Code/C#/CardProcessingApi/CardProcessingApi.Web/Controllers/MerchantController.cs
@@ -1,7 +1,18 @@
-﻿using System.Web.Http;
-using CardProcessingApi.Core;
-using CardProcessingApi.Web.Framework.Filters;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using AutoMapper;
 using CardProcessing.Business.BusinessLogic.MerchantLogic;
+using CardProcessingApi.Core;
+using CardProcessingApi.Data;
+using CardProcessingApi.Web.Framework;
+using CardProcessingApi.Web.Framework.Extension;
+using CardProcessingApi.Web.Framework.Filters;
+using CardProcessingApi.Web.Models;
 
 namespace CardProcessingApi.Web.Controllers
 {
@@ -33,14 +44,14 @@ namespace CardProcessingApi.Web.Controllers
             return Ok();
         }
 
-        //[HttpGet]
-        //[Route("all")]
-        //public List<MerchantBindingModels> GetAll()
-        //{
-        //    var merchants = _merchantLogic.GetAll();
-        //    var entities = Mapper.Map<List<MerchantListItemModel>>(merchants);
+        [HttpGet]
+        [Route("all")]
+        public List<MerchantListItemModel> GetAll()
+        {
+            var merchants = _merchantLogic.GetAll();
+            var entities = Mapper.Map<List<MerchantListItemModel>>(merchants);
 
-        //    return entities;
-        //}
+            return entities;
+        }
     }
 }
